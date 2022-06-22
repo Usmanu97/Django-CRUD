274cff3d6531db16042d632403a3f6853747c635
Usman Abubakar <abubakarusman2010@yahoo.com>
2022-06-22 07:28:19 +0100

    initial commit

---
+++ blog/views.py
@@ -0,0 +1,35 @@
+from venv import create
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView,DeleteView
+from django.views.generic.detail import DetailView
+from .models import Post
+from django.urls import reverse_lazy
+from django.views import View
+
+from blog.models import Post
+
+# Create your views here.
+
+class PostListView(ListView): 
+    model = Post
+
+class PostCreateView(CreateView):
+     model = Post 
+     fields = "__all__"
+     success_url = reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostUpdateView(UpdateView):
+     model = Post
+     fields = "__all__"
+     success_url = reverse_lazy("blog:all")
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
